5b16bf01a3e279244e1c4aa273424412700bb980
kristian3 <kristianjanevski@yahoo.com>
2018-12-24 12:20:49 +0100

    Create java1.js

---
+++ Java c1/java1.js
@@ -0,0 +1,89 @@
+// soberisopet //
+// soberi so pet // snake case
+// soberiSoPet // camel case
+function soberiSoPet (a) {
+    var y = 5;
+    console.log(a + y);
+}
+
+soberiSoPet(1);
+soberiSoPet(5);
+soberiSoPet(6);
+soberiSoPet(3);
+soberiSoPet(21);
+soberiSoPet(65);
+soberiSoPet(24);
+soberiSoPet(58);
+soberiSoPet(32);
+soberiSoPet(44);
+soberiSoPet(23);
+
+
+
+
+
+
+
+function soberiSoTri (a) {
+    var y = 3;
+    console.log(a + y);
+}
+
+soberiSoTri(17);
+soberiSoTri(3);
+soberiSoTri(100);
+
+function soberi(a, b) {
+    console.log(a + b);
+}
+
+soberi(2, 6);
+soberi(3, 3);
+soberi(33, 3);
+
+
+
+
+
+
+
+
+function f2c(f){
+    //console.log((f - 32) * 5/9);
+    return (f - 32) * 5/9;
+}
+
+function c2f(c){
+    //console.log(c * 9/5 + 32);
+    return c * 9/5 + 32;
+}
+
+var c = f2c(100); // fahrenheit -> celsius
+console.log(c)
+var f =  c2f(28); // celsius -> fahrenheit
+console.log(f);
+
+
+
+
+
+//definiranje funkcii
+
+var fn1 = function(){    //anonimna funkcija
+    console.log('fn1');
+}
+
+var fn2 = () => {       //asdasdasdas
+    console.log('fn2');
+}
+
+// mario povtoruvanje  
+
+
+
+
+
+
+
+
+
