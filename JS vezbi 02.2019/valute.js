ddfdff851a64de7e50ffde7627b6ab483d9f04e8
kristian3 <kristianjanevski@yahoo.com>
2019-02-12 00:03:47 +0100

    Create valute.js

---
+++ JS vezbi 02.2019/valute.js
@@ -0,0 +1,16 @@
+var amount = document.querySelector("#amount");
+var currency = document.querySelector("#curency");
+var button = document.querySelector("#convert");
+var result = document.querySelector("#result");
+
+
+button.addEventListener("click", function() {
+    switch(currency.value) {
+        case "eur":
+        result.innerHTML = "MKD: " + (parseInt(amount.value) * 61.5)
+        break;
+        case "usd":
+        result.innerHTML = "MKD: " + (parseInt(amount.value) * 44.5)
+        break;
+    }
+})
