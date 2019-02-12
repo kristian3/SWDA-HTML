659289c8922b687771286b51e6fc253f838ff8e8
kristian3 <kristianjanevski@yahoo.com>
2019-02-12 12:49:39 +0100

    Create povtoruvanje.js

---
+++ JS vezbi 02.2019/povtoruvanje.js
@@ -0,0 +1,13 @@
+
+
+
+var duva = false;
+var vrne = true;
+
+if(vrne && duva) {
+    console.log('ponesi montka');
+ } else if(duva) {
+    console.log('ponesi kaput');
+} else if(vrne){
+    console.log('ponesi cador');
+}
